3e0962b4303f7e0171df573e72742e716f4a6b4d
Cameron Johnston <cameron@needle.com>
2011-06-23 17:34:39 -0600

    convert boolean and int values in attributes/mongodb.rb to strings for now, since the empty? method used on them in mongodb.conf.erb template does not work on booleans and ints

--- attributes/mongodb.rb
+++ attributes/mongodb.rb
@@ -23,62 +23,62 @@ default[:mongodb][:server][:dbpath]                = "/var/lib/mongodb"
 default[:mongodb][:server][:dir]                   = "/opt/mongodb-#{mongodb[:version]}"
 default[:mongodb][:server][:logpath]               = "/var/log/mongodb.log"
 default[:mongodb][:server][:pidfile]               = "/var/lib/mongodb/mongod.lock"
-default[:mongodb][:server][:port]                  = 27017
+default[:mongodb][:server][:port]                  = "27017"
 default[:mongodb][:server][:system_init]           = "sysv"
 
 ### EXTRA
-default[:mongodb][:server][:auth]                  = false
-default[:mongodb][:server][:cpu]                   = false
-default[:mongodb][:server][:diaglog]               = false
-default[:mongodb][:server][:logappend]             = true
-default[:mongodb][:server][:nocursors]             = false
-default[:mongodb][:server][:nohints]               = false
-default[:mongodb][:server][:nohttpinterface]       = false
-default[:mongodb][:server][:noscripting]           = false
-default[:mongodb][:server][:notablescan]           = false
-default[:mongodb][:server][:noprealloc]            = false
-default[:mongodb][:server][:nssize]                = false
-default[:mongodb][:server][:objcheck]              = false
+default[:mongodb][:server][:auth]                  = "false"
+default[:mongodb][:server][:cpu]                   = "false"
+default[:mongodb][:server][:diaglog]               = "false"
+default[:mongodb][:server][:logappend]             = "true"
+default[:mongodb][:server][:nocursors]             = "false"
+default[:mongodb][:server][:nohints]               = "false"
+default[:mongodb][:server][:nohttpinterface]       = "false"
+default[:mongodb][:server][:noscripting]           = "false"
+default[:mongodb][:server][:notablescan]           = "false"
+default[:mongodb][:server][:noprealloc]            = "false"
+default[:mongodb][:server][:nssize]                = "false"
+default[:mongodb][:server][:objcheck]              = "false"
 default[:mongodb][:server][:password]              = ""
-default[:mongodb][:server][:quota]                 = false
+default[:mongodb][:server][:quota]                 = "false"
 default[:mongodb][:server][:username]              = ""
-default[:mongodb][:server][:verbose]               = false
+default[:mongodb][:server][:verbose]               = "false"
 
 ### STARTUP
-default[:mongodb][:server][:rest]                  = false
-default[:mongodb][:server][:syncdelay]             = 60
+default[:mongodb][:server][:rest]                  = "false"
+default[:mongodb][:server][:syncdelay]             = "60"
 
 ### MMS
-default[:mongodb][:server][:mms]                   = false
+default[:mongodb][:server][:mms]                   = "false"
 default[:mongodb][:server]['mms-interval']         = ""
 default[:mongodb][:server]['mms-name']             = ""
 default[:mongodb][:server]['mms-token']            = ""
 
 ### REPLICATION
-default[:mongodb][:server][:autoresync]            = false
-default[:mongodb][:server][:master]                = false
+default[:mongodb][:server][:autoresync]            = "false"
+default[:mongodb][:server][:master]                = "false"
 default[:mongodb][:server][:master_source]         = ""
-default[:mongodb][:server][:opidmem]               = 0
-default[:mongodb][:server][:oplogsize]             = 0
-default[:mongodb][:server][:replication]           = false
+default[:mongodb][:server][:opidmem]               = "0"
+default[:mongodb][:server][:oplogsize]             = "0"
+default[:mongodb][:server][:replication]           = "false"
 default[:mongodb][:server][:replSet]               = ""
-default[:mongodb][:server][:slave]                 = false
+default[:mongodb][:server][:slave]                 = "false"
 default[:mongodb][:server][:slave_only]            = ""
 default[:mongodb][:server][:slave_source]          = ""
 
 ### SHARDING
-default[:mongodb][:server][:shard_server]          = false
+default[:mongodb][:server][:shard_server]          = "false"
 
 ### BACKUP
 default[:mongodb][:server][:backup][:backupdir]    = "/var/backups/mongodb"
 default[:mongodb][:server][:backup][:cleanup]      = "yes"
 default[:mongodb][:server][:backup][:compression]  = "bzip2"
-default[:mongodb][:server][:backup][:day]          = 6
+default[:mongodb][:server][:backup][:day]          = "6"
 default[:mongodb][:server][:backup][:host]         = "localhost"
 default[:mongodb][:server][:backup][:latest]       = "yes"
-default[:mongodb][:server][:backup][:mailaddress]  = false
+default[:mongodb][:server][:backup][:mailaddress]  = "false"
 default[:mongodb][:server][:backup][:mailcontent]  = "stdout"
-default[:mongodb][:server][:backup][:maxemailsize] = 4000
+default[:mongodb][:server][:backup][:maxemailsize] = "4000"
 
 
 
@@ -89,8 +89,8 @@ default[:mongodb][:config_server][:config]  = "/etc/mongodb-config.conf"
 default[:mongodb][:config_server][:datadir] = "/var/db/mongodb-config"
 default[:mongodb][:config_server][:logpath] = "/var/log/mongodb-config.log"
 default[:mongodb][:config_server][:pidfile] = "/var/run/mongodb-config.pid"
-default[:mongodb][:config_server][:port]    = 27019
-default[:mongodb][:config_server][:verbose] = false
+default[:mongodb][:config_server][:port]    = "27019"
+default[:mongodb][:config_server][:verbose] = "false"
 
 
 
@@ -100,5 +100,5 @@ default[:mongodb][:mongos][:bind_ip] = bind_ip
 default[:mongodb][:mongos][:config]  = "/etc/mongos.conf"
 default[:mongodb][:mongos][:logpath] = "/var/log/mongos.log"
 default[:mongodb][:mongos][:pidfile] = "/var/run/mongos.pid"
-default[:mongodb][:mongos][:port]    = 27017
-default[:mongodb][:mongos][:verbose] = false
+default[:mongodb][:mongos][:port]    = "27017"
+default[:mongodb][:mongos][:verbose] = "false"
